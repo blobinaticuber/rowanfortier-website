9739b8e30e04fdf9c58ae9a1ba65857016f292fb
Rowan Fortier <rowan.fortier@gmail.com>
2026-03-20 20:24:07 -0700

    fix capitalization

--- my-react-app/src/App.jsx
+++ my-react-app/src/App.jsx
@@ -1,7 +1,7 @@
 import { useState } from 'react'
 import './App.css'
 import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
-import { Home } from './pages/home'
+import { Home } from './pages/Home'
 import { Projects } from './pages/Projects'
 
 import { Header } from './components/header'
