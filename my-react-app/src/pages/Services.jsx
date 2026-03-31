9518566445ea388adf8c5e373058ca926b2785f6
Rowan Fortier <rowan.fortier@gmail.com>
2026-03-31 00:39:19 -0700

    add servicecard component

--- my-react-app/src/pages/Services.jsx
+++ my-react-app/src/pages/Services.jsx
@@ -1,40 +1,39 @@
-import { Card } from "../components/Card"
+// import { Card } from "../components/Card"
+import { ServiceCard } from "../components/ServiceCard";
 import { Footer } from "../components/Footer"
 
-import './Services.css'
+import './Services.css';
 
 export function Services() {
     return (
         <>
         <h1>Services</h1>
-        <p>Need a simple website? I got you covered!</p>
+        <p>Need a simple static website? I got you covered!</p>
+
         <div className="grid3">
-            <Card color={"var(--blue)"}>
-                <div class="vstack">
-                    <h2>Basic</h2>
-                    <ul>
-                        <li>dogs</li>
-                    </ul>
-                    <h2>$150</h2>
-                </div>
-            </Card>
 
-            <Card color={"var(--pink)"}>
-                <div class="vstack">
-                    <h2>Basic</h2>
-                    <p>a</p>
-                    <h2>$100</h2>
-                </div>
-            </Card>
+            <ServiceCard color={"var(--green)"}>
+                <h2>Basic</h2>
+                <ul>
+                    <li>1-3 page website</li>
+                    <li>4 months of support</li>
+                </ul>
+                <h2 className="price">CAD $149.99</h2>
+            </ServiceCard>
 
-            <Card color={"var(--yellow)"}>
-                <div class="vstack">
-                    <h2>Basic</h2>
-                    <p>a</p>
-                    <h2>$100</h2>
-                </div>
-            </Card>
+            <ServiceCard color={"var(--pink)"}>
+                <h2>EXTREME</h2>
+                <ul>
+                    <li>3-5 page website</li>
+                    <li>Lifetime support</li>
+                    <li>Run Google ads and earn revenue</li>
+                </ul>
+                <h2 className="price">CAD $249.99</h2>
+            </ServiceCard>
+
         </div>
+
+        <p>Not convinced? See the Projects page for examples of my work!</p>
 
         <Footer/>
         </>
