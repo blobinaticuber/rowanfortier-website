fc53662a7b96959209fd802a45d9c082d09a00d4
Rowan Fortier <rowan.fortier@gmail.com>
2026-04-30 19:57:05 -0700

    add filter and filter dropdown UI

--- my-react-app/src/pages/Projects.jsx
+++ my-react-app/src/pages/Projects.jsx
@@ -3,6 +3,8 @@ import { Card } from "../components/Card"
 import './Projects.css'
 import { ImageShape } from "../components/ImageShape"
 
+import { FaFilter } from "react-icons/fa6";
+
 import {useState, useEffect} from 'react';
 
 export function Projects() {
@@ -11,6 +13,8 @@ export function Projects() {
     const [loading, setLoading] = useState(true);
     const [search, setSearch] = useState("");
     const [filteredData, setFilteredData] = useState([]);
+
+    const [filterOpen, setFilterOpen] = useState(false);
 
     useEffect(() => {
         fetch('projects.json')
@@ -45,7 +49,18 @@ export function Projects() {
     return (
         <>
             <h1>Projects</h1>
-            <input onKeyDown={searchUpdate} onInput={searchUpdate} type="text" placeholder="Search..."></input>
+            <div className="controls">
+                <button className="filter-button" onClick={()=> setFilterOpen(!filterOpen)}><FaFilter /> Filter</button>
+                <div className={ filterOpen ? "filter-dropdown-open" : "filter-dropdown"}>
+                    <p>Website ({data.filter(c => c.categories.includes("Website")).length})</p>
+                    <p>Hackathon ({data.filter(c => c.categories.includes("Hackathon")).length})</p>
+                    <p>Browser extension ({data.filter(c => c.categories.includes("Browser extension")).length})</p>
+                    <p>Program ({data.filter(c => c.categories.includes("Program")).length})</p>
+                    <p>Mod ({data.filter(c => c.categories.includes("Mod")).length})</p>
+                </div>
+                <input onKeyDown={searchUpdate} onInput={searchUpdate} type="text" placeholder="Search..."></input>
+            </div>
+
                 <div className="grid">
                 {filteredData.length>0 ? filteredData.map((projects) => (
                     <Card className="project-card" key={projects.key} link={projects.link} color={projects.color}>
