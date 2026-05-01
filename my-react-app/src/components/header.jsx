f01a26458cc424d9cacfe5668c68148557a9ff7b
Rowan Fortier <rowan.fortier@gmail.com>
2026-04-30 19:13:47 -0700

    change header nav icon

--- my-react-app/src/components/header.jsx
+++ my-react-app/src/components/header.jsx
@@ -2,14 +2,14 @@ import './header.css'
 import { BrowserRouter as Router, Routes, Route, Link, NavLink } from 'react-router-dom';
 import { ImHome3 } from "react-icons/im";
 import { MdOutlineWeb } from "react-icons/md";
-import { BsEaselFill } from "react-icons/bs";
+import { PiSquaresFourFill } from "react-icons/pi";
 
 export function Header() {
     return (
         <div className="topnav">
             <div className="nav">
                 <NavLink to="/"><ImHome3/><p className="linkText">Home</p></NavLink>
-                <NavLink to="/projects"><BsEaselFill/><p className="linkText">Projects</p></NavLink>
+                <NavLink to="/projects"><PiSquaresFourFill /><p className="linkText">Projects</p></NavLink>
                 <NavLink to="/services"><MdOutlineWeb/><p className="linkText">Services</p></NavLink>
             </div>
         </div>
