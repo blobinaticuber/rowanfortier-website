b34afbcece2ec7f98a74f27dd16c08ccae3c2c46
Rowan Fortier <rowan.fortier@gmail.com>
2026-05-27 22:47:32 -0700

    Add featured projects to home page

--- my-react-app/src/pages/Home.jsx
+++ my-react-app/src/pages/Home.jsx
@@ -6,15 +6,57 @@ export function Home() {
     return (
         <>
             <h1>Rowan Fortier</h1>
-            <Card color="var(--yellow)">
-                <div className="hstack">
-                    <ImageShape border shape="square" path="/rowan.png"/>
-                    <div className="vstack">
-                        <h2>About me</h2>
-                        <p>I'm a Computer Science student studying at the University of British Columbia. My interests are in web and app development, and a bit of computer graphics and game development. Some personal hobbies/interests I have are photography, astronomy, juggling, and twisty puzzles.</p>
+
+            {/* <div className="vstack"> */}
+                <Card color="var(--yellow)">
+                    <div className="hstack">
+                        <ImageShape border shape="square" path="/rowan.png"/>
+                        <div className="vstack">
+                            <h2>About me</h2>
+                            <p>I'm a Computer Science student studying at the University of British Columbia. My interests are in web and app development, and a bit of computer graphics and game development. Some personal hobbies/interests I have are photography, astronomy, juggling, and twisty puzzles.</p>
+                        </div>
                     </div>
+                </Card>
+
+                <h2>Featured Projects</h2>
+
+                <div className="grid3">
+
+                    <Card color="var(--red)" link="https://decimaltime.rowanfortier.com/">
+                        <div className="vstack">
+                                <ImageShape path="/projects/decimaltime.svg"/>
+                                <div className="vstack">
+                                    <h2>Decimal Time</h2>
+                                </div>
+                        <p>Explores a system of telling time based on decimally related units.</p>
+                        </div>
+                    </Card>
+
+                    <Card color="var(--purple)" link="https://hypercubing.xyz/">
+                        <div className="vstack">
+                                <ImageShape path="/projects/HSC2.svg"/>
+                                <div className="vstack">
+                                    <h2>Hypercubing.xyz</h2>
+                                </div>
+                        <p>Wiki and leaderboard system for the hypercubing community.</p>
+                        </div>
+                    </Card>
+
+                    <Card color="var(--gray)" link="https://github.com/blobinaticuber/3x3x3-simulator">
+                        <div className="vstack">
+                                <ImageShape shape="squircle" path="/projects/processing-3x3x3.png"/>
+                                <div className="vstack">
+                                    <h2>3x3x3 Simulator</h2>
+                                </div>
+                        <p>Rubik's Cube simulator that uses linear algebra for graphics and puzzle state.</p>
+                        </div>
+                    </Card>
+
                 </div>
-            </Card>
+
+
+            {/* </div> */}
+
             <Footer/>
         </>
     )
